4b9b979698a9d540d2e7afc25faa3543b13ae8fa
shreejupradhan <shreejupradhan39@gmail.com>
2021-11-14 16:54:08 +0545

    About us vay

---
+++ assets/php/aboutus.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Products</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+    <script src="https://kit.fontawesome.com/d03c35338f.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+
+    <div class="header">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="../../images/logo.png" width="130px" height="68px">
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="../../index.php">Home</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="aboutus.php">About</a></li>
+                        <li><a href="contactus.php">Contact</a></li>
+                        <li><a href="signup.php">Login/Register</a></li>
+                    </ul>
+                </nav>
+                <img src="../../images/cart1.png" width="30px" height="30px">
+            </div>
+        </div>
+    </div>
+
+    <!---products--->
+    <div class="" style="height: 65vh;padding:10px 40px;font-size: 1.2rem">
+        <h1 class="title">About Us</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+    <!--footer-->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col-1">
+                    <ul>
+                        <a href="">
+                            <h2>About us</h2>
+                        </a>
+                        <a href="">
+                            <h2>Our services</h2>
+                        </a>
+                    </ul>
+                </div>
+                <div class="footer-col-2">
+                    <h3>Get help</h3>
+                    <ul>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">shipping</a></li>
+                        <li><a href="">payment options</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col-3">
+                    <h3>Follow Us</h3>
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
